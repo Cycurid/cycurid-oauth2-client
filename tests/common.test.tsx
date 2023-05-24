cf6bc6ba7fd02dcf186192359417697f2a6963b4
Jordan <developer@cycurid.com>
2023-05-24 14:49:47 -0700

    updated tests

--- tests/common.test.tsx
+++ tests/common.test.tsx
@@ -3,10 +3,18 @@ import { render } from '@testing-library/react'
 
 import 'jest-canvas-mock'
 
-import { Barcode } from '../src'
+import Barcode from '../src/components/Barcode'
 
 describe('Common render', () => {
   it('renders without crashing', () => {
-    render(<Barcode />)
+    const config = {
+      clientID: '5HOm4THg9vWRG0MZFy0s165o',
+      redirectURL: 'https://2125bb20a4f4.ngrok.app/login',
+      scope: ['reference_uuid', 'first_name'],
+      action: 'login',
+      entityName: 'test merchant',
+      size: 200,
+    }
+    render(<Barcode config={config} />)
   })
 })
